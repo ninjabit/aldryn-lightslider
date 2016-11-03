d242f20fbf5e584817e78abef64ec7d232d5c907
Tobia Ghiraldini <tobia.ghiraldini@ninjabit.com>
2016-11-03 17:02:35 +0100

    Adding tox support

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 import os
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
